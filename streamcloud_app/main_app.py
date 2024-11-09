d440d7a01844a50f0ed242d11b53b4125e24ac47
tarralo <128602868+tarralo@users.noreply.github.com>
2024-11-09 14:57:14 -0300

    Update main_app.py

--- streamcloud_app/main_app.py
+++ streamcloud_app/main_app.py
@@ -108,7 +108,7 @@ def main():
         pop_col1, pop_col2, pop_col3 = st.columns(3, gap="large")
         pop_col1.image("capes.png", caption = "CAPES - Coordination for the Improvement of Higher Education Personnel", width=200)
         pop_col2.image("ufu.png", caption = "UFU - Federal University of Uberlândia", width=200)
-        pop_col3.image("ppgeelt.png", caption = "PPGEELT - Post-Graduation Electrical Engineering Program", width=200) 
+        pop_col3.image("ppgeelt.png", caption = "PPGEELT - Post-Graduation Electrical Engineering Program", width=250) 
     uploaded_file = st.file_uploader('Choose a .csv file with relay data', accept_multiple_files=False, type='csv') # the user insert the .csv file with the relay data
     if uploaded_file is not None:
         try: relay_data = pd.read_csv(uploaded_file, encoding='utf-8', sep=';', decimal=',') # read the .csv file
