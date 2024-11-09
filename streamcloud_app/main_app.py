8fe39c2b96f1875a6b4133e5f0b1f71dd48e2de7
tarralo <128602868+tarralo@users.noreply.github.com>
2024-11-09 14:25:22 -0300

    Update main_app.py

--- streamcloud_app/main_app.py
+++ streamcloud_app/main_app.py
@@ -104,11 +104,11 @@ def main():
         st.write('**Uberlândia, Brazil**')
         st.markdown('[LinkedIn](https://www.linkedin.com/company/leapse-ufu/)')
         st.markdown('[Instagram](https://www.instagram.com/leapse.ufu/)')
-    '''with st.popover("Supported by 👨‍🏫"):
-        pop_col1, pop_col2, pop_col3 = st.columns(3, gap="large")
-        pop_col1.image("capes.png", caption = "CAPES - Coordination for the Improvement of Higher Education Personnel", width=200)
-        pop_col2.image("ufu.png", caption = "UFU - Federal University of Uberlândia", width=200)
-        pop_col3.image("ppgeelt.png", caption = "PPGEELT - Post-Graduation Electrical Engineering Program", width=200) '''
+    #with st.popover("Supported by 👨‍🏫"):
+    #    pop_col1, pop_col2, pop_col3 = st.columns(3, gap="large")
+    #    pop_col1.image("capes.png", caption = "CAPES - Coordination for the Improvement of Higher Education Personnel", width=200)
+    #    pop_col2.image("ufu.png", caption = "UFU - Federal University of Uberlândia", width=200)
+    #    pop_col3.image("ppgeelt.png", caption = "PPGEELT - Post-Graduation Electrical Engineering Program", width=200) 
     uploaded_file = st.file_uploader('Choose a .csv file with relay data', accept_multiple_files=False, type='csv') # the user insert the .csv file with the relay data
     if uploaded_file is not None:
         try: relay_data = pd.read_csv(uploaded_file, encoding='utf-8', sep=';', decimal=',') # read the .csv file
