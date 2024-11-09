11ddc82719472349db9bb23ad2cc61e847bf803c
tarralo <128602868+tarralo@users.noreply.github.com>
2024-11-09 13:55:15 -0300

    Update main_app.py

--- streamcloud_app/main_app.py
+++ streamcloud_app/main_app.py
@@ -99,20 +99,20 @@ def main():
         st.markdown('[ResearchGate](https://www.researchgate.net/profile/Wellington-Maycon-S-Bernardes)')
     col1.markdown('[GitHub Repository](https://github.com/tarralo/ansi51_blockchain)')
     col1.write('**Last Update** 08 November 2024')
-    leapse_logo = Image.open(r"leapse.png")
-    col2.image(leapse_logo, use_column_width='auto') 
+    #leapse_logo = Image.open(r"leapse.png")
+    col2.image("leapse.png", use_column_width='auto') 
     with col2.popover("**:green[Laboratory of Alternative Energies and Protection of Electrical Systems]**"):
         st.write('**Uberlândia, Brazil**')
         st.markdown('[LinkedIn](https://www.linkedin.com/company/leapse-ufu/)')
         st.markdown('[Instagram](https://www.instagram.com/leapse.ufu/)')
     with st.popover("Supported by 👨‍🏫"):
         pop_col1, pop_col2, pop_col3 = st.columns(3, gap="large")
-        capes_logo = Image.open(r"streamcloud_app/capes.png")
-        pop_col1.image(capes_logo, caption = "CAPES - Coordination for the Improvement of Higher Education Personnel", width=200)
-        ufu_logo = Image.open(r"ufu.png")
-        pop_col2.image(ufu_logo, caption = "UFU - Federal University of Uberlândia", width=200)
-        ppgeelt_logo = Image.open(r"ppgeelt.png")
-        pop_col3.image(ppgeelt_logo, caption = "PPGEELT - Post-Graduation Electrical Engineering Program", width=200) 
+        #capes_logo = Image.open(r"streamcloud_app/capes.png")
+        pop_col1.image("capes.png", caption = "CAPES - Coordination for the Improvement of Higher Education Personnel", width=200)
+        #ufu_logo = Image.open(r"ufu.png")
+        pop_col2.image("ufu.png", caption = "UFU - Federal University of Uberlândia", width=200)
+        #ppgeelt_logo = Image.open(r"ppgeelt.png")
+        pop_col3.image("ppgeelt.png", caption = "PPGEELT - Post-Graduation Electrical Engineering Program", width=200) 
     uploaded_file = st.file_uploader('Choose a .csv file with relay data', accept_multiple_files=False, type='csv') # the user insert the .csv file with the relay data
     if uploaded_file is not None:
         try: relay_data = pd.read_csv(uploaded_file, encoding='utf-8', sep=';', decimal=',') # read the .csv file
