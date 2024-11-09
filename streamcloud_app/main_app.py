5c65f97e6a9257d34b3830a4d92524b71779ac3f
tarralo <128602868+tarralo@users.noreply.github.com>
2024-11-09 14:01:56 -0300

    Update main_app.py

--- streamcloud_app/main_app.py
+++ streamcloud_app/main_app.py
@@ -98,7 +98,7 @@ def main():
         st.markdown('[ResearchGate](https://www.researchgate.net/profile/Wellington-Maycon-S-Bernardes)')
     col1.markdown('[GitHub Repository](https://github.com/tarralo/ansi51_blockchain)')
     col1.write('**Last Update** 08 November 2024')
-    col2.image("leapse.png", use_column_width='auto') 
+    render_image("leapse.png") 
     with col2.popover("**:green[Laboratory of Alternative Energies and Protection of Electrical Systems]**"):
         st.write('**Uberlândia, Brazil**')
         st.markdown('[LinkedIn](https://www.linkedin.com/company/leapse-ufu/)')
