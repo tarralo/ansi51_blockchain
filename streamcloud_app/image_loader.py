c0815336ef1c24d559937b33f83f0404bc6cece0
tarralo <128602868+tarralo@users.noreply.github.com>
2024-11-09 14:06:08 -0300

    Update image_loader.py

--- streamcloud_app/image_loader.py
+++ streamcloud_app/image_loader.py
@@ -7,7 +7,7 @@ def render_image(filepath: str):
    """
    mime_type = filepath.split('.')[-1:][0].lower()
    with open(filepath, "rb") as f:
-   content_bytes = f.read()
-   content_b64encoded = base64.b64encode(content_bytes).decode()
-   image_string = f'data:image/{mime_type};base64,{content_b64encoded}'
-   st.image(image_string)
+      content_bytes = f.read()
+      content_b64encoded = base64.b64encode(content_bytes).decode()
+      image_string = f'data:image/{mime_type};base64,{content_b64encoded}'
+      st.image(image_string)
